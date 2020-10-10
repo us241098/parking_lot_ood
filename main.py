addf91d062a0b16fc6ea60a22f980f85836f5ee2
utsav <us241077@gmail.com>
2020-10-10 16:36:34 +0530

    docstrings, comments, proper formatted

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
 from models import ParkingLot, AGE_SLOT, AGE_VEHICLE, SLOT_CAR, Vehicle
 
 if __name__ == "__main__":
+    # driver program to take input from the input file and generate a output #
     
-    output_file="output.txt"
-    read_input=open("input.txt", "r")
+    output_file="output.txt"       # output file
+    read_input=open("input.txt", "r")  # input file
     writer = open(output_file, "w")
-    
     lines = read_input.readlines()
 
     for i in lines:
-        #print (i)
         i=i.replace("\n", "")
         steps = i.split(" ")
-        print (steps)
         
         if len(steps)==2:
             if steps[0]=='Create_parking_lot':
                 lot = ParkingLot(int(steps[1]))
+                assert int(steps[1])>0
                 output_line= f"Creating parking of {steps[1]} slots \n"
                 
             if steps[0]=='Leave':
                 car, age=lot.clear_space(int(steps[1]))
-                output_line= f"Slot number {steps[1]} vacated, the car with vehicle registration number {car} left the space, the driver of the car was of age {age} \n"
+                output_line= f"Slot number {steps[1]} vacated, the car with vehicle registration number \"{car}\" left the space, the driver of the car was of age {age} \n"
 
             if steps[0]=='Slot_numbers_for_driver_of_age':
                 output_slots=AGE_SLOT[str(steps[1])]
-                output_slots = list(map(int, output_slots)) 
-                output_line=str(output_slots)[1:-1]   + "\n"
+                output_slots = list(map(int, output_slots))
+                
+                if len(output_slots)==0:
+                    output_line="null"   + "\n"
+                else:
+                    output_line=str(output_slots)[1:-1]   + "\n"
                 
             if steps[0]=='Vehicle_registration_number_for_driver_of_age':
-                print(AGE_VEHICLE)
-                output_cars=AGE_VEHICLE[str(steps[1])] 
-                output_line=str(output_cars) + "\n"
-                print(AGE_VEHICLE[str(199)])
+                output_cars=AGE_VEHICLE[str(steps[1])]
+                if len(output_cars)==0:
+                    output_line="null"  + "\n"
+                else:    
+                    output_line=str(output_cars) + "\n"
                 
             if steps[0]=='Slot_number_for_car_with_number':
                 for key, value in SLOT_CAR.items():
                     if value==steps[1]:    
-                        #output_slots=AGE_SLOT[str(steps[1])]
-                        output_line=str(key)  + "\n"
-                
-                                
-                
-            
+                        output_line=str(key)  + "\n"       
         
         elif len(steps)==4:
             task=steps[0]
@@ -52,7 +51,7 @@ if __name__ == "__main__":
             if task =="Park":
                 vehicle=Vehicle(car_number, int(driver_age))
                 slot_assigned=vehicle.park(lot)
-                output_line= f"Car with vehicle registration number {car_number} has been parked at slot number {slot_assigned} \n"
+                output_line= f"Car with vehicle registration number \"{car_number}\" has been parked at slot number {slot_assigned} \n"
                 
             
         print(output_line)
--- models.py
+++ models.py
@@ -2,41 +2,61 @@ import collections
 import heapq
 
 # Since database is not used in this activity, I have used these dictionary to emulate a DB
-AGE_VEHICLE=collections.defaultdict(list)
-SLOT_CAR={}
-AGE_SLOT=collections.defaultdict(list)
+AGE_VEHICLE=collections.defaultdict(list) # age-> car_num (list) map
+SLOT_CAR={} # slot_num-> car_num mapr
+AGE_SLOT=collections.defaultdict(list) # age-> slot_num (list) map
 
 
 class ParkingLot:
+    # constructor for parking lot
     def __init__(self, size):
         self.size=size
         self.slots={}
         self.available_slots=[i for i in range(1, size+1)]
         heapq.heapify(self.available_slots)
-        
+
+    # free the parking slot
     def clear_space(self, slot_num):
-        heapq.heappush(self.available_slots,slot_num)
-        car_number=SLOT_CAR[str(slot_num)]
+        '''
+        Frees the parking space and returns car_number and age of the driver.
+
+        Parameters:
+                slot_num (int): number of slot to be freed.
+                self (ParkingLot): parking lot from where slot is being freed.
+
+        Returns:
+                car_number (str): vehicle registration number of the removed vehicle.
+                age_key (str): age of the driver who is leaving the lot.
+        '''
+        try:
+            # add the slot back to the heap
+            heapq.heappush(self.available_slots,slot_num)
+            car_number=SLOT_CAR[str(slot_num)]
+            
+            # remove slot_num from SLOT_CAR map
+            del SLOT_CAR[str(slot_num)]
+            
+            # remove car_number from AGE_VEHICLE
+            for key, values in AGE_VEHICLE.items():
+                if str(car_number) in values:
+                    age_key=key
+                    vehicle_list=AGE_VEHICLE[str(age_key)]
+                    vehicle_list=vehicle_list.remove(car_number)
+                    AGE_VEHICLE[str(age_key)]=vehicle_list
+            
+            # remove slot_num from AGE_SLOT
+            for key, values in AGE_SLOT.items():
+                if str(slot_num) in values:
+                    age_key=key
+                    slot_list=AGE_SLOT[str(age_key)]
+                    slot_list=slot_list.remove(str(slot_num))
+                    AGE_SLOT[str(age_key)]=slot_list
+                    
+            return car_number,age_key
         
-        del SLOT_CAR[str(slot_num)]
-        
-        # remove car_number from AGE_VEHICLE
-        for key, values in AGE_VEHICLE.items():
-            if str(car_number) in values:
-                age_key=key
-                vehicle_list=AGE_VEHICLE[str(age_key)]
-                vehicle_list=vehicle_list.remove(car_number)
-                AGE_VEHICLE[str(age_key)]=vehicle_list
-        
-        # remove slot_num from AGE_SLOT
-        for key, values in AGE_SLOT.items():
-            if str(slot_num) in values:
-                age_key=key
-                slot_list=AGE_SLOT[str(age_key)]
-                slot_list=slot_list.remove(str(slot_num))
-                AGE_SLOT[str(age_key)]=slot_list
-                
-        return car_number,age_key
+        except:
+            # TODO make sure duplicates can't be pushed in the heap
+            print("Space already not occupied")
 
 
 
@@ -44,33 +64,27 @@ class Vehicle:
     def __init__(self, vehicle_number, driver_age):
         self.vehicle_number=vehicle_number
         self.driver_age=driver_age
-        
+
+    # park the vehicle
     def park(self, ParkingLotObj):
+        '''
+        Parks the car in the parking lot and returns the slot assigned.
+
+        Parameters:
+                ParkingLotObj (ParkingLot): ParkingLot object where car is to be parked.
+                self (Vehicle): Vehicle object to be parked.
+
+        Returns:
+                slot_assigned (int): slot number assigned to the vehicle.
+        '''
         if len(ParkingLotObj.available_slots)==0:
             print("No spaces available in our parking lot")
+            return -1
         
-        slot_assigned=heapq.heappop(ParkingLotObj.available_slots)
-        SLOT_CAR[str(slot_assigned)]=self.vehicle_number
-        AGE_VEHICLE[str(self.driver_age)].append(self.vehicle_number)
-        AGE_SLOT[str(self.driver_age)].append(str(slot_assigned))
-        
-        return slot_assigned
-'''       
-lot = ParkingLot(4)
-
-vehicle1=Vehicle("jkfdsd", 23)
-vehicle1.park(lot)
-vehicle2=Vehicle("jkfdsfsd", 21)
-vehicle2.park(lot)
-
-print(AGE_VEHICLE)
-print(SLOT_CAR)
-print(AGE_SLOT)
-
-lot.clear_space(1)
-
-print(AGE_VEHICLE)
-print(SLOT_CAR)
-print(AGE_SLOT)
-
-'''
+        else:
+            slot_assigned=heapq.heappop(ParkingLotObj.available_slots)
+            SLOT_CAR[str(slot_assigned)]=self.vehicle_number
+            AGE_VEHICLE[str(self.driver_age)].append(self.vehicle_number)
+            AGE_SLOT[str(self.driver_age)].append(str(slot_assigned))
+            
+            return slot_assigned
